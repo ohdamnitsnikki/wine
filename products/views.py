640e4300918eaebcd847e6e1d1a176f2fb5bd12f
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-06-18 20:22:03 +0000

    add product category on products

--- products/views.py
+++ products/views.py
@@ -46,10 +46,13 @@ def all_products(request):
                 name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
