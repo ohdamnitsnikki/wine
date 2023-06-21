751436f5141c6813fff1f7ef09950b0dde900d25
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-06-21 21:06:28 +0000

    att templatetags

--- bag/views.py
+++ bag/views.py
@@ -15,11 +15,12 @@ def add_to_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
 
+    bag = request.session.get('bag', {})
 
-if item_id in list(bag.keys()):
-    bag[item_id] += quantity
-else:
-    bag[item_id] = quantity
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
