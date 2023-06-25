8978af4784eae9c1f9d1a21b5b900b01e3abd3dd
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-06-25 18:20:12 +0000

    add secret and public key

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,10 @@ def checkout(request):
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
