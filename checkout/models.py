86de1cf43c886735476febcfe3ab3db05a7c01e1
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-06-25 10:40:40 +0000

    add def for saving to database

--- checkout/models.py
+++ checkout/models.py
@@ -59,3 +59,14 @@ class Order(models.Model):
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the lineitem total
+        and update the order total.
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
