1ccde615177876af1f02788efb38ff3f9cc559f0
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-06-25 18:02:44 +0000

    add js to stripe element

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51N9lfGEdpCg6RolQoM4WY2BHMUdVQpuORSHjEh0ex2IBWpMsCr2SxTkannF6xqaIHimcUAcWAFhAXIWsCTFcULBA00YpSDyhxt',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
