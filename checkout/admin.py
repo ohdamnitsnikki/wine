cc2a45323705a4227ae1e85007a2ef7b7dde7bd0
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-06-26 20:04:26 +0000

    add new model fields to admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -22,7 +22,8 @@ class OrderAdmin(admin.ModelAdmin):
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
-                    'grand_total',)
+                    'grand_total', 'original_bag',
+                    'stripe_pid')
 
     ordering = ('-date',)
 
