3b04348d923ff87ff07125163871f39cab5ddcef
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-06-29 18:09:40 +0000

    create profile app and add model for user model

--- checkout/models.py
+++ checkout/models.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from django_countries.fields import CountryField
 
 from products.models import Product
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
