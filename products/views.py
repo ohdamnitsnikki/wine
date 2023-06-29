71c4cfc108d227a5891d7a668011def414b8f456
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-06-29 20:32:58 +0000

    add def add products

--- products/views.py
+++ products/views.py
@@ -69,3 +69,15 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
