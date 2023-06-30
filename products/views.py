fc39c52554967b86a25762601d70297a20323679
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-06-30 20:02:03 +0000

    update add product

--- products/views.py
+++ products/views.py
@@ -79,14 +79,14 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
         form = ProductForm()
-        
+
     template = 'products/add_product.html'
     context = {
         'form': form,
