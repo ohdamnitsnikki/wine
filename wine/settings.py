6409ef277a9aadd0929125d8db5b9df93eb793e0
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-07-11 20:55:40 +0000

    remove secret key and change debug

--- wine/settings.py
+++ wine/settings.py
@@ -25,12 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-o8l8u+z&@=(g(=$gnc@%7$k(7&z6#ki_)=rt$^f4=_px)46d^+'
-)
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'wine-oclock-d9434a8420f0.herokuapp.com', '8000-ohdamnitsnikki-wine-tg6nppvxgpf.ws-eu101.gitpod.io',
